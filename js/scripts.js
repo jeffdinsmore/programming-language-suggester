260b50b79e1ac168cb78c62f35773ca844a0a0cf
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-14 15:04:28 -0700

    add one function for business logic and added 5 constants for the quesitons with an if statement in user interface logic in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 // Business logic
 
+function cSharp (num1, num2, num3) {
+  if (Math.round((num1 + num2 + num3)/3) === 1) {
+    return true
+  }
+}
+
 $(document).ready(function() {
   $("button#submit").click(function() {
     event.preventDefault();
 
 
-// User interface logig
+// User interface logic
     const flavor = $("input:[name=exampleradios]:checked").val();
 
-    const question1 = $("select#question1").val();
+    const question1Input = $("select#question1").val();
+    const question2Input = $("select#question1").val();
+    const question3Input = $("select#question1").val();
+    const question4Input = $("select#question1").val();
+    const question5Input = $("select#question1").val();
 
-    if ( === "") {
-      
-    } else if ( === "") {
+    if (cSharp(question1Input, question2Input, question3Input, question4Input, question5Input)) {
+      $("#result").html("C#");
+    } else if ( joe === "") {
       $("#result-riker").show();
     }
     
