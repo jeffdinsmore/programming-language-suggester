dcfcc8a5e60a0fed0947ca1192b4af1eab638dc5
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-14 15:23:12 -0700

    update the constants in javascript to reflect the questions in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,29 @@
 // Business logic
 
-function cSharp (num1, num2, num3) {
-  if (Math.round((num1 + num2 + num3)/3) === 1) {
+function cSharp (num1, num2, num3, num4, num5) {
+  if (Math.round((num1 + num2 + num3 + num4 + num5)/5) === 1) {
     return true
   }
 }
 
 $(document).ready(function() {
-  $("button#submit").submit(function() {
+  $("button#submit").submit(function(event) {
     event.preventDefault();
 
 
 // User interface logic
 
-    const question1Input = $("select#question1").val();
-    const question2Input = $("select#question1").val();
-    const question3Input = $("select#question1").val();
-    const question4Input = $("select#question1").val();
-    const question5Input = $("select#question1").val();
+    const question1Input = parseInt$("#question1").val();
+    const question2Input = parseInt$("#question2").val();
+    const question3Input = parseInt$("#question3").val();
+    const question4Input = parseInt$("#question4").val();
+    const question5Input = parseInt$("#question5").val();
 
     if (cSharp(question1Input, question2Input, question3Input, question4Input, question5Input)) {
-      $("#result").html("C#");
+      $("#result").text("C#");
+      $("form#form").hide();
     } else if ( joe === "") {
-      $("#result-riker").show();
+      $("#story").show();
     }
     
   });
