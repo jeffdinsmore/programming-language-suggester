9b6cb12efd04d5e7160080eb99b8e274cb66c112
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-21 20:43:43 -0700

    add error message and logic in js if a user does not make a selection. updated values for please select option to be zero

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,9 @@ $(document).ready(function() {
     const question5Input = parseInt($("#question5").val());
     const inputtedName = $("#name").val();
     
-    if (cSharp(question1Input, question2Input, question3Input, question4Input, question5Input)) {
+    if (question1Input === 0 || question2Input === 0 || question3Input === 0 || question4Input === 0 || question5Input === 0) {
+      $("#result").text("Please answer all the questions before submitting");
+    } else if (cSharp(question1Input, question2Input, question3Input, question4Input, question5Input)) {
       $("#result").text("C#");
       $("#story").show();
       $("#form").hide();
