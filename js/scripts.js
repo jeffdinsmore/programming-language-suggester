501b33625333eb60fdfbc72ccbb4dd92f501ef3f
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-22 21:44:00 -0700

    added question6Input to user interface logic and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 // Business logic
 
-function cSharp (num1, num2, num3, num4, num5) {
-  if (Math.round((num1 + num2 + num3 + num4 + num5)/5) === 1) {
+function cSharp (num1, num2, num3, num4, num5, num6) {
+  if (Math.round((num1 + num2 + num3 + num4 + num5 + num6)/6) === 1) {
     return true
   }
 }
 
-function Go (num1, num2, num3, num4, num5) {
-  if (Math.round((num1 + num2 + num3 + num4 + num5)/5) === 2) {
+function Go (num1, num2, num3, num4, num5, num6) {
+  if (Math.round((num1 + num2 + num3 + num4 + num5 + num6)/6) === 2) {
     return true
   }
 }
 
-function Rust (num1, num2, num3, num4, num5) {
-  if (Math.round((num1 + num2 + num3 + num4 + num5)/5) === 3) {
+function Rust (num1, num2, num3, num4, num5, num6) {
+  if (Math.round((num1 + num2 + num3 + num4 + num5 + num6)/6) === 3) {
     return true
   }
 }
 
-function Python (num1, num2, num3, num4, num5) {
-  if (Math.round((num1 + num2 + num3 + num4 + num5)/5) > 3) {
+function Python (num1, num2, num3, num4, num5, num6) {
+  if (Math.round((num1 + num2 + num3 + num4 + num5 + num6)/6) > 3) {
     return true
   }
 }
@@ -40,15 +40,16 @@ $(document).ready(function() {
     
     if (question1Input === 0 || question2Input === 0 || question3Input === 0 || question4Input === 0 || question5Input === 0 || question6Input === 0 || inputtedName.length < 1) {
       alert("Please answer all the questions before submitting");
-    } else if (cSharp(question1Input, question2Input, question3Input, question4Input, question5Input)) {
+    } else if (cSharp(question1Input, question2Input, question3Input, question4Input, question5Input, question6Input)) {
       $("#result").text("C#");
+      $("#animate").show();
       $("#story").show();
       $("#form").hide();
       $("button#secondary").show();
       $("#nameInput").text(inputtedName);
       $("#complete").show();
       $("#fillOut").hide();
-    } else if (Go(question1Input, question2Input, question3Input, question4Input, question5Input)) {
+    } else if (Go(question1Input, question2Input, question3Input, question4Input, question5Input, question6Input)) {
       $("#result").text("GO");
       $("#story").show();
       $("#form").hide();
@@ -56,7 +57,7 @@ $(document).ready(function() {
       $("#nameInput").text(inputtedName);
       $("#complete").show();
       $("#fillOut").hide();
-    } else if (Rust(question1Input, question2Input, question3Input, question4Input, question5Input)) {
+    } else if (Rust(question1Input, question2Input, question3Input, question4Input, question5Input, question6Input)) {
       $("#result").text("Rust");
       $("#story").show();
       $("#form").hide();
@@ -64,7 +65,7 @@ $(document).ready(function() {
       $("#nameInput").text(inputtedName);
       $("#complete").show();
       $("#fillOut").hide();
-    } else if (Python(question1Input, question2Input, question3Input, question4Input, question5Input)) {
+    } else if (Python(question1Input, question2Input, question3Input, question4Input, question5Input, question6Input)) {
       $("#result").text("Python");
       $("#story").show();
       $("#form").hide();
